9a3cc6331a04a3e8f88be678b37e5308b386cf97
Anand Desai <desaiaj@rose-hulman.edu>
2023-09-23 20:58:17 -0400

    Added meta tags to svelte:head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,19 @@
-<script>
-	import '../app.css';
-</script>
+<svelte:head>
+	<title>Multi Step Form | Frontend Mentor</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta
+		name="description"
+		content="A multi-step form featuring navigation, validation and advanced frontend fundamentals"
+	/>
+	<meta name="keywords" content="form, validation, frontend mentor" />
+	<meta name="robots" content="index,follow" />
+	<meta name="author" content="Anand Desai" />
+	<meta name="googlebot" content="index,follow" />
+	<meta name="referrer" content="no-referrer" />
+	<meta name="color-scheme" content="dark light" />
+	<link rel="stylesheet" type="text/css" href="/src/app.css" />
+</svelte:head>
 
 <slot />
