277044fd5aedc71b0d29338613d330ed22824f32
Anand Desai <desaiaj@rose-hulman.edu>
2023-09-27 03:57:56 -0400

    Moved assets to

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<div class="container mx-auto">
-    Hello
+<script>
+    import desktopSidebar from "$lib/images/bg-sidebar-desktop.svg";
+    import mobileSidebar from "$lib/images/bg-sidebar-mobile.svg";
+</script>
+<div class="grid grid-cols-1 place-items-center h-screen bg-magnolia justify-center items-center">
+    <div class="w-3/5 aspect-video rounded-3xl bg-alabaster drop-shadow-2xl shadow-cool-gray">
+        <picture>
+            <source class="sm" srcset={mobileSidebar} />
+            <source class="lg" srcset={desktopSidebar} />
+            <img src={desktopSidebar} alt="desktop sidebar"/>
+            </picture>
+        </div>
 </div>
 
