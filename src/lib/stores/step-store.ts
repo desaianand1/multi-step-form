651b9819453ba5471477f9192458cf84e37c8282
Anand Desai <desaiaj@rose-hulman.edu>
2023-10-28 02:02:16 -0400

    added core functionality to step store

--- src/lib/stores/step-store.ts
+++ src/lib/stores/step-store.ts
@@ -1,4 +1,27 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
-const DEFAULT_STEP = 1;
-export const step = writable<number>(DEFAULT_STEP);
+const INITIAL_STEP = 1;
+const FINAL_STEP = 4;
+export const step = writable<number>(INITIAL_STEP);
+
+export function advanceStep() {
+	step.update((step) => {
+		if (step + 1 > FINAL_STEP) {
+			return step;
+		}
+		return step + 1;
+	});
+}
+
+export function resetSteps() {
+	step.set(INITIAL_STEP);
+}
+
+export function revertStep() {
+	step.update((step) => {
+		if (step - 1 < INITIAL_STEP) {
+			return step;
+		}
+		return step - 1;
+	});
+}
