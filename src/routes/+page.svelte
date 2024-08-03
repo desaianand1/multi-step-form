6962ae7de441ce719c50bf43e700e1b5eea1d568
Anand Desai <desaiaj@rose-hulman.edu>
2024-08-03 13:29:00 -0400

    chore: added comment for context on app layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
 	import PersonalInfo from "$lib/components/modules/personal-info.svelte"
 </script>
+
+<!-- Default first module to display. See /layouts for the semantics for the wrapping card layout-->
 <PersonalInfo />
